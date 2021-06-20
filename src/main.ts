69272ea9ccb01c3e21e6bf5bed3a3ad882eca4f2
Marios Balamatsias <mbalamatsias@gmail.com>
2021-06-20 11:43:58 +0300

    Send hello to keftes
    Signed-off-by: Marios Balamatsias <mbalamatsias@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,21 @@ console.log("Hello Marablu!");
 const encoder = new TextEncoder();
 const decoder = new TextDecoder();
 
-const _peers: string[] = [
+interface Peer {
+  hostname: string;
+  port: number;
+}
+
+const peers: string[] = [
   "keftes.di.uoa.gr:18018",
 ];
 
-const handleConnection = async (connection: Deno.Conn) => {
+const parsePeerFromString = (_peer: string) => {
+  // TODO implement, validate inputs, check if addresses are indeed addresses
+  return { hostname: "keftes.di.uoa.gr", port: 18018 } as Peer;
+};
+
+const handleIncomingConnection = async (connection: Deno.Conn) => {
   const buffer = new Uint8Array(1024);
   await connection.read(buffer);
   console.log("Received: ", decoder.decode(buffer));
@@ -14,10 +24,26 @@ const handleConnection = async (connection: Deno.Conn) => {
   connection.close();
 };
 
-const hostname = "0.0.0.0";
-const port = 18018;
-const listener = Deno.listen({ hostname, port });
-console.log(`Listening on ${hostname}:${port}`);
-for await (const conn of listener) {
-  handleConnection(conn);
-}
+const _startListener = async () => {
+  const hostname = "0.0.0.0";
+  const port = 18018;
+  const listener = Deno.listen({ hostname, port });
+  console.log(`Listening on ${hostname}:${port}`);
+  for await (const conn of listener) {
+    handleIncomingConnection(conn);
+  }
+};
+
+const connectToPeer = async () => {
+  console.log("connecting to peer");
+  const { hostname, port } = parsePeerFromString(peers[0]);
+  const con = await Deno.connect({ hostname, port });
+  await con.write(encoder.encode('{"type":"hello"}'));
+  const buf = new Uint8Array(1024);
+  await con.read(buf);
+  console.log("Peer - Response:", decoder.decode(buf));
+  // con.close();
+};
+
+// startListener();
+connectToPeer();
